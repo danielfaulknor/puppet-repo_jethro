2ee909d333b84fdfa2de9ca07a733ae79f2ef9ba
Dan Faulknor <dan@internetbydesign.nz>
2016-06-13 05:27:33 -0400

    Fixing changed config

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,7 @@ class repo_netbydesign {
     apt::source { 'ibd':
         location   => "http://apt.netbydesign.nz/debian/",
         repos      => 'main',
-        key        => 'E12EA1180C929E7F2133C83E6393734839DA8F29',
-        key_source => 'puppet://module/repo_netbydesign/netbydesign_signing.gpg.key'
+	key 	   => { 'source' => 'https://apt.netbydesign.nz/debian/ibd_dan_signing.gpg.key' }
     }
 
 }
