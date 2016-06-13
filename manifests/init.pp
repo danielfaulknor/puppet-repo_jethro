5d6f497d973ab6c30d8803c17cbbfa1b106fd321
Dan Faulknor <dan@internetbydesign.nz>
2016-06-13 05:14:39 -0400

    Further fixes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,20 +1,11 @@
 # Sets up the repositories for apt.netbydesign.nz
 class repo_netbydesign {
 
-  if ( $::osfamily == 'Debian' ) {
-
     apt::source { 'ibd':
         location   => "http://apt.netbydesign.nz/debian/",
         repos      => 'main',
         key        => 'E12EA1180C929E7F2133C83E6393734839DA8F29',
         key_source => 'puppet://module/repo_netbydesign/netbydesign_signing.gpg.key'
     }
-
-    }
-  } else{
-    notify { "repo_netbydesign not supported for your platform.":
-      loglevel => debug,
-    }
-  }
 
 }
